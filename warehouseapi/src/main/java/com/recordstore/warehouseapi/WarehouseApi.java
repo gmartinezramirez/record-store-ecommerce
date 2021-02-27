405671719a29bf672fbae1d9d102e4592c09db11
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2021-02-27 17:04:06 -0300

    feat(warehouse-api): add exclusion for junit-vintage

--- warehouseapi/src/main/java/com/recordstore/warehouseapi/WarehouseApi.java
+++ warehouseapi/src/main/java/com/recordstore/warehouseapi/WarehouseApi.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WarehouseApi {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WarehouseApi.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApi.class, args);
+    }
 
 }
